3b9d279ca974aebb0af0f563759dee91aef48721
Even Stensberg <evenstensberg@gmail.com>
2023-03-16 14:20:59 +0100

    syntax sugar: use destructuring

--- main.mjs
+++ main.mjs
@@ -8,11 +8,11 @@ const jsonList = fs.readFileSync('./filmer.json', 'utf8')
 const list = JSON.parse(jsonList)
 
 if (argv.list) {
-  list.forEach((element) => {
+  list.forEach(element => {
     console.log(
       `Film: ${chalk.green.bold(element.name)}, Utgiver: ${chalk.green.bold(
-        element.source,
-      )}`,
+        element.source
+      )}`
     )
   })
   process.exit(1)
@@ -22,21 +22,20 @@ if (argv.random) {
   const film = list[Math.floor(Math.random() * list.length)]
   console.log(
     `Fant film: ${chalk.green.bold(film.name)}, Finnes: ${chalk.green.bold(
-      film.source,
-    )}`,
+      film.source
+    )}`
   )
   process.exit(1)
 }
 
 if (argv.add) {
-  const navn = argv.navn
-  const kilde = argv.kilde
+  const { navn, kilde } = argv
   list.push({ name: navn, source: kilde })
   fs.writeFileSync('./filmer.json', JSON.stringify(list, null, 4), 'utf8')
   process.exit(1)
 }
 if (argv.names) {
-  list.forEach((element) => {
+  list.forEach(element => {
     console.log(element.name)
   })
   process.exit(1)
